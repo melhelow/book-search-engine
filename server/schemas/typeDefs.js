58319256dba388f88dc63542e2d12fd32c94ba0e
Mohamed <114217025+melhelow@users.noreply.github.com>
2023-06-22 01:47:02 -0500

    backend, index.js, resolvers, typedefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,45 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+
+ type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]!
+    }
+    
+    type Book {
+    _id: ID
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+    
+    }
+
+    type Query {
+    me: User
+    }
+    type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(authors: [String], description: String, bookId: String, image: String, link: String, title: String): User
+    removeBook(bookId: String!): User
+    }
+
+    type Auth {
+    token: ID!
+    user: User
+    }
+
+
+
+
+`;
+
+
+
+module.exports = typeDefs;
